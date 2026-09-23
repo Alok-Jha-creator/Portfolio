9ab9f5d78396d21a917a89bc33154632964e1747
Alok Jha <ajha49993@gmail.com>
2026-09-24 00:29:39 +0545

    update

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -32,11 +32,8 @@ export default function About() {
             whileHover={{ scale: 1.02 }}
             transition={{ type: "spring", stiffness: 200, damping: 18 }}
             aria-hidden="true"
-          >
-            {/* Replace with your actual avatar image */}
-            
+          > 
             <div className="absolute inset-0 " />
-           
             <img src={p} alt="test" />
           </motion.div>
 
